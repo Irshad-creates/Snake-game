cf7557db3d171196ceee37bf6f1d4dbeac388f19
Irshad-creates <ansariirshad78601@gmail.com>
2025-12-04 23:58:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let board = document.querySelector('.board');
+const blockHeight = 30
+const blockWidth = 30
+
+const cols = Math.floor(board.clientHeight/blockHeight);
+const rows = Math.floor(board.clientWidth / blockWidth);
+
+for(let i = 0 ; i < rows*cols ; i++){
+    const block = document.createElement('div');
+    block.classList.add('block');
+
+    board.appendChild(block);
+}
