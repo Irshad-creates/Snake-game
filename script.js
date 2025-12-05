ba55bb950ab48288e074fc603c9fddc0d7a7108b
Irshad-creates <ansariirshad78601@gmail.com>
2025-12-05 22:01:49 +0530

    completed the base of the game

--- script.js
+++ script.js
@@ -1,13 +1,78 @@
-let board = document.querySelector('.board');
-const blockHeight = 30
-const blockWidth = 30
+let board = document.querySelector(".board");
+const blockHeight = 80;
+const blockWidth = 80;
 
-const cols = Math.floor(board.clientHeight/blockHeight);
-const rows = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
+const cols = Math.floor(board.clientWidth / blockWidth);
+const blocks = {};
 
-for(let i = 0 ; i < rows*cols ; i++){
-    const block = document.createElement('div');
-    block.classList.add('block');
+let direction = "right";
 
+let snake = [
+  { x: 1, y: 4 },
+  // ,{
+  //     x:1,y: 5,
+  // },{
+  //     x :1 ,y : 6
+  // }
+];
+
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
     board.appendChild(block);
+    block.innerText = `${row}-${col}`;
+    blocks[`${row}-${col}`] = block;
+  }
 }
+
+function render() {
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+  });
+}
+
+setInterval(() => {
+
+    let head  = null
+
+    if(direction === 'left'){
+        head = {x : snake[0].x, y : snake[0].y - 1}
+    }else if (direction === 'right'){
+        head = {x : snake[0].x, y : snake[0].y + 1}
+    }else if ( direction === 'up'){
+        head = {x : snake[0].x - 1, y : snake[0].y}
+    }else if ( direction === 'down'){
+        head = {x : snake[0].x + 1, y : snake[0].y}
+    }
+
+    snake.forEach(segment =>{
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill')
+    })
+    snake.unshift(head)
+    snake.pop()
+
+    render()
+}, 300);
+
+addEventListener("keydown", function (event) {
+  if ((event.key === "ArrowUp" || event.key === "w") && direction !== "down") {
+    direction = "up";
+  } else if (
+    (event.key === "ArrowDown" || event.key === "s") &&
+    direction !== "up"
+  ) {
+    direction = "down";
+  } else if (
+    (event.key === "ArrowRight" || event.key === "d") &&
+    direction !== "left"
+  ) {
+    direction = "right";
+  } else if (
+    (event.key === "ArrowLeft" || event.key === "a") &&
+    direction !== "right"
+  ) {
+    direction = "left";
+  }
+});
